f21eeb879aa407087de85d13db7c92236bfe4f80
HongBinKim <hongbinkim@gmail.com>
2014-09-08 16:23:41 +0900

    Rejected commit: Default

--- app/src/main/java/com/hbk/sinaweibodemo/util/Constants.java
+++ app/src/main/java/com/hbk/sinaweibodemo/util/Constants.java
@@ -24,6 +24,7 @@ package com.hbk.sinaweibodemo.util;
  */
 public interface Constants {
 
+
     /** 当前 DEMO 应用的 APP_KEY，第三方应用应该使用自己的 APP_KEY 替换该 APP_KEY */
     public static final String APP_KEY      = "308955607";
 
